96ce81ce5816e3ab022ce8a4780291dfbd1964eb
yuanxx <824264439@qq.com>
2018-07-27 17:34:43 +0800

    整合Vuex Signed-off-by: yuanxx <824264439@qq.com>

---
+++ src/store/state.js
@@ -0,0 +1,23 @@
+/*
+状态对象模块
+ */
+export default {
+// 当前桌位在线用户列表
+  userInfos: [{
+    userName: '美队',
+    tableNo: '01',
+    iconUrl: '/static/headIcon/captain-america.png'
+  }, {
+    userName: '忍者',
+    tableNo: '01',
+    iconUrl: '/static/headIcon/ninja.png'
+  }, {
+    userName: '小黄人',
+    tableNo: '01',
+    iconUrl: '/static/headIcon/minion.png'
+  }, {
+    userName: '小鸟',
+    tableNo: '01',
+    iconUrl: '/static/headIcon/angry-birds.png'
+  }]
+}
