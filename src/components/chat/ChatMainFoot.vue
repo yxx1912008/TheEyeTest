8d34a2a2549296c815c8e9cd49655b69839f756e
yuanxx <824264439@qq.com>
2018-08-01 09:01:48 +0800

    前后端正式进行整合

---
+++ src/components/chat/ChatMainFoot.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="chat-footer">
+    <el-form :inline="true" size="medium">
+      <el-form-item>
+        <el-input placeholder="请输入信息"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">发送</el-button>
+      </el-form-item>
+    </el-form>
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+    methods: {
+      onSubmit: function () {
+
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .chat-footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 3.5rem;
+    border-top: thin solid gray;
+    background-color: white;
+  }
+
+  .el-form {
+    margin-top: 0.8rem;
+  }
+</style>
---
+++ src/components/constant/Config.js
@@ -0,0 +1,4 @@
+/*
+常用配置
+ */
+export const WEBSOCKET_URL = 'ws://192.168.18.124:8080/luckydeer/websocket/'
